6003e7f51c06c9e7d80479cc5af33bcccd68297b
BHAVANA SAHU <bvasahu@gmail.com>
2026-03-25 22:41:01 +0530

    Create 1pattern.java

---
+++ 1pattern.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+ 1      for(int i=0;i<4;i++){
+           for (int j=0;j<4;j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
