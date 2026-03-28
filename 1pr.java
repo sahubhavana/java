e1cc52ff11aaccb07fad185a09c7bb57ce85d95f
BHAVANA SAHU <bvasahu@gmail.com>
2026-03-28 22:51:13 +0530

    Create 1pr.java

---
+++ 1pr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class newbegning {
+    public static void main(Strings args[]){
+        System.out.print("Hello world");
+        System.out.println("Hello from me to intellij idea");
+       int a=10;
+       int b=90;
+     System.out.println("Sum of two number "+(a+b));
+        Scanner sc=new Scanner(System.in);
+           String name=sc.nextLine();
+
+           System.out.println(name);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int sum=a+b;
+       System.out.println("the sum of two number="+sum);
+
+
+    }
+}
