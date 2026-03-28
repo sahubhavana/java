ede00a9f08c263d47be3266298fcd5b5c826c25f
BHAVANA SAHU <bvasahu@gmail.com>
2026-03-28 22:52:06 +0530

    Create maxelement.java to input array elements
    Added a Java program to read 8 elements into an array.

---
+++ maxelement.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class maxelement {
+    public static void main(Strings[] args) {
+        Scanner sc=new Scanner(System.in);
+        int [] array=new int[8];
+        for(int i=0;i<8;i++){
+            System.out.println("enter a element of array");
+            array[i]= sc.nextInt();
+
+        }
+
+
+    }
+
+}
