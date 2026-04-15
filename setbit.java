13804065815d404a8e1ae636c7f4acddd09c8b1d
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-15 11:38:03 +0530

    Create setbit.java

---
+++ setbit.java
@@ -0,0 +1,17 @@
+  public static int setbit(int a,int b){
+// # //        String c=Integer.toBinaryString(a);
+// # # //        String d=Integer.toBinaryString(b);
+// # # //        int countc=0;
+// # # //        int countd=0;
+// # # //        for(int i=0;i<c.length();i++){
+// # # //            if(c.charAt(i)=='1'){
+// # # //                countc=countc+1;
+// # # //            }
+// # # //        }
+// # # //        for(int i=0;i<d.length();i++) {
+// # # //            if (d.charAt(i) == '1') {
+// # # //                countd = countd + 1;
+// # # //            }
+// # # //        }
+// # # //        return(countc+countd);
+// # # //    }
