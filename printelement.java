b11ac8e4ad114e9e09299bde658db942bc28d00c
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-15 11:34:07 +0530

    Create printelement.java

---
+++ printelement.java
@@ -0,0 +1,11 @@
+ printing of element with the help of for loop
+       for(int i=0;i<a.length;i++){
+           System.out.println(a[i]);
+       }
+Taking input from user with the help of for loop
+       for(int i=3;i<a.length;i++){
+           a[i]=sc.nextInt();
+       }
+   for(int i=0;i<a.length;i++){
+       System.out.println(a[i]);
+   }
