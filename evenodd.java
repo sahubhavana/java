b609b59d42ce471ae1c0536f67465b2b8bf0bbc4
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-15 11:35:18 +0530

    Uncomment checkevenodd method in Function class

---
+++ evenodd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Function {
+  public static  void checkevenodd(int a){
+       if(a%2==0){
+           System.out.println("Number is even");
+       }
+       else{
+           System.out.println("Numbaer is odd");
+       }
+   }
