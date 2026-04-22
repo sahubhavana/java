05da728b3eadc1552ebddf1e5e046ffe9f3eac1d
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-22 23:31:15 +0530

    Add union of two arrays functionality
    Implement union of two arrays using a HashSet to eliminate duplicates.

---
+++ unionoftwoarray.cpp
@@ -0,0 +1,18 @@
+27.union of two arrays
+        int nums1[] = {1, 2, 3, 4, 5};
+        int nums2[] = {1, 2, 7};
+        Set<Integer> set1 = new HashSet<>();
+
+        for (int x : nums1) {
+            set1.add(x);
+        }
+        for (int x : nums2) {
+            set1.add(x);
+        }
+        int[] arr = new int[set1.size()];
+        int i = 0;
+        for (int x : set1) {
+            arr[i++] = x;
+        }
+        for (int j= 0; j < arr.length; j++) {
+            System.out.print(arr[j]+" ");
