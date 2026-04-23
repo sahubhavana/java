de300b8c62b5818b700316a9b161c0ffd633f9ff
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-23 22:31:19 +0530

    Create sumallelemnt.java

---
+++ sumallelemnt.java
@@ -0,0 +1,12 @@
+ 1.Take a array a print the sum of all element  that can present in array
+       int arr[];
+        arr=new int[10];
+        for(int i=0;i<arr.length;i++){
+            System.out.println("enter a value=");
+            arr[i]=sc.nextInt();
+        }
+       int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=sum+arr[i];
+        }
+        System.out.println("sum of all the number="+sum);
