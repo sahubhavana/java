a86ce73ba761e3c51b8b3529c630e978d80c0d83
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-29 23:00:13 +0530

    Create Pattern13.java

---
+++ Pattern13.java
@@ -0,0 +1,10 @@
+12.
+        int sum=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(sum+" ");
+                sum=sum+1;
+            }
+            System.out.println();
+        }
+    }
