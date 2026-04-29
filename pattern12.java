dafdae29f6c3b6327a162f1611ef5c46d5a35d0c
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-29 23:00:42 +0530

    Create pattern12.java

---
+++ pattern12.java
@@ -0,0 +1,12 @@
+for(int i=1;i<=n;i++){
+           for(int j=1;j<=i;j++){
+               System.out.print(j);
+           }
+           for(int k=0;k<n-i;k++){
+               System.out.print(" ");
+           }
+           for(int l=i;l>=1;l--){
+               System.out.print(l);
+           }
+           System.out.println();
+       }
