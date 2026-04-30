fd60d205182dd23fa0a6baf53b61f023036bb1a8
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-30 13:43:48 +0530

    Create vowelcount.java

---
+++ vowelcount.java
@@ -0,0 +1,7 @@
+ 3.Count a vowel in  string
+    int vowelcount=0;
+    for(int i=0;i<str.length();i++){
+        if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
+            vowelcount++;
+        }
+    }
