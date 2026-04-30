036aa79074b0ee2f6fb26500c732b9f810299f53
BHAVANA SAHU <bvasahu@gmail.com>
2026-04-30 13:43:12 +0530

    Create countlength.java

---
+++ countlength.java
@@ -0,0 +1,6 @@
+2.Count a length of string without using   string .length() function
+       int count=0;
+       for(int i=0;i<str.length();i++){
+           count=count+1;
+       }
+       System.out.print("length of string="+count);
