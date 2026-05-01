8992984f646d2f52828b336779dab05bb3c54df3
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-01 11:57:30 +0530

    Create wordinsentence.java

---
+++ wordinsentence.java
@@ -0,0 +1,8 @@
+11.Count a word in  sentence
+
+       String[] words = str.trim().split("\\s+");
+
+       // Count words
+       int count = words.length;
+
+       System.out.println("Number of words: " + count);
