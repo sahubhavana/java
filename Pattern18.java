eb38e24a6aab2ffef6535c725d90bcb0e31eb3f8
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-04 22:20:02 +0530

    Add commented pattern generation code in Pattern18.java

---
+++ Pattern18.java
@@ -0,0 +1,26 @@
+ 18.
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            for(int k=n-i-1;k>0;k--){
+//                System.out.print("  ");
+//            }
+//            for(int j=0;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=0;i<n-1;i++){
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print("*");
+//            }
+//            for(int k=0;k<=i;k++){
+//                System.out.print("  ");
+//
+//            }
+//            for(int j=0;j<n-i-1;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
