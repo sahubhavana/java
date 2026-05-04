69d8bbbe21808ff35f23c5e66953e476f0f74f36
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-04 22:18:38 +0530

    Create Pattern19.java

---
+++ Pattern19.java
@@ -0,0 +1,15 @@
+19.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // boundary condition
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    
+
