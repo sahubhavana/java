204bc61f6fe334e053f564e6911f78fb79de545a
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-04 22:20:36 +0530

    Create Pattern17.java

---
+++ Pattern17.java
@@ -0,0 +1,27 @@
+17.
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<n-i;j++){
+//                System.out.print("*");
+//            }
+//            for(int k=0;k<i;k++){
+//                System.out.print("  ");
+//            }
+//            for(int j=0;j<n-i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            for(int k=n-i-1;k>0;k--){
+//                System.out.print("  ");
+//            }
+//            for(int j=0;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+
+
