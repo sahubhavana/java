85517d51df8255a4df89528afef4e6d9999f37cb
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-05 22:52:03 +0530

    Create check anagaram.java

---
+++ check anagaram.java
@@ -0,0 +1,19 @@
+16.To check whether is anagram or not
+//        String s="dog";
+//        String t="cat";
+//        boolean ch=true;
+//        int[] arr=new int[25];
+//
+//        if(s.length()!=t.length()) System.out.print(false);
+//        for(int i=0;i<s.length();i++){
+//            arr[s.charAt(i) - 'a']++;   // increment
+//            arr[t.charAt(i) - 'a']--;   //
+//
+//        }
+//        for(int i=0;i<arr.length;i++){
+//            if(arr[i]!=0){
+//                ch=false;
+//                break;
+//            }
+//        }
+//        System.out.print(ch);
