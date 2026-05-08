16baf8a16402c15a6011fc520f641ede748efb4f
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-08 21:44:23 +0530

    Create memodigicount.java

---
+++ memodigicount.java
@@ -0,0 +1,10 @@
+// 2.To print a digit count
+
+    public static int Countdig(int n, int[] dp) {
+        if (n == 0) return 0;
+
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+
+        return dp[n] = 1 + Countdig(n / 10, dp);
