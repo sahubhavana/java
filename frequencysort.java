d1000bf889722d5365bbfe2d9a886c20451fb7a9
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-09 22:29:08 +0530

    Create frequencysort.java

---
+++ frequencysort.java
@@ -0,0 +1,14 @@
+  public List<Character> frequencySort(String s) {
+        List<Character> list = new ArrayList<>();
+        int [] arr=new int [26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+
+        }
+        for(int i=0;i<26;i++){
+            if(arr[i]>0){
+                char ch=(char)(i+'a');
+                 list.add(ch);
+            }
+        }
+        return list;
