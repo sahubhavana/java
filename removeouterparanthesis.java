ace8990e3632bfeb54606ec05e60dc10c62aaf41
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-09 22:31:00 +0530

    Add method to remove outer parentheses
    Implement a method to remove outer parentheses from a string.

---
+++ removeouterparanthesis.java
@@ -0,0 +1,15 @@
+ public String removeOuterParentheses(String s) {
+       String str="";
+       int count=0;
+       for(int i=0;i<s.length();i++){
+         if(s.charAt(i)==')'){
+            count--;
+         }
+         if(count!=0) {
+            str=str+s.charAt(i);
+         }
+         if(s.charAt(i)=='(')
+         count++;
+        
+       }
+         return str;
