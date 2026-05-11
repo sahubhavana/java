81892193acc01291502b1514a3c9e380059f61e0
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-11 22:14:37 +0530

    Create removechar.java

---
+++ removechar.java
@@ -0,0 +1,11 @@
+/ to remove a character from string
+       String s = "Banana";
+       String result = "";
+       char s2 = 'a';
+       for (int i = 0; i < s.length(); i++) {
+           if (s.charAt(i) != s2) {
+               result += s.charAt(i);
+           }
+       }
+
+       System.out.println(result);
