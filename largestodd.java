a4749a37790570ebb7fd7eaf5bdec83a6ecb1bde
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-14 15:18:36 +0530

    Create largestodd.java

---
+++ largestodd.java
@@ -0,0 +1,21 @@
+
+class Solution {    
+    public String largeOddNum(String s) {
+       String  result="";
+       int n=s.length();
+       for(int i=n-1;i>0;i--){
+        if((s.charAt(i)-'0')%2!=0){
+            result=result+s.substring(0,i+1);
+            break;
+        }
+        i=0;
+        while(i<result.length() && result.charAt(i)=='0'){
+            i++;
+        }
+        result=result.substring(i);
+        return result;
+         
+    }  
+
+    }
+}
