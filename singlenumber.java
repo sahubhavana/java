a70810cfabd4b87883489a20b143fada79b0c0e1
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-15 13:36:04 +0530

    Create singlenumber.java

---
+++ singlenumber.java
@@ -0,0 +1,13 @@
+public int singleNumber(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count=count+1;
+                }
+            }
+            if(count==1){
+                return nums[i];
+
+            }
+            count=0;
