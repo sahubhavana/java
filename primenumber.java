8b481bbb2c91d06fd5379fc774b21df224258bdd
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-18 22:28:52 +0530

    Create primenumber.java

---
+++ primenumber.java
@@ -0,0 +1,17 @@
+6.To check that number is prime or not
+//        boolean check = true;
+//        if (num == 1) {
+//            check = true;
+//
+//        } else {
+//            for (int i = 2; i < num - 1; i++) {
+//                if (num % i == 0) {
+//                    check = false;
+//                }
+//            }
+//        }
+//        if (check == true) {
+//            System.out.print("number is prime");
+//        } else {
+//            System.out.print("number is not prime");
+//        }   
