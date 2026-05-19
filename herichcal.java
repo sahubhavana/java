be28029853588d1c070f216e5fae852aeb1a1965
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-19 22:37:13 +0530

    Create herichcal.java

---
+++ herichcal.java
@@ -0,0 +1,18 @@
+/ 3.Heririechal inheritance implementation
+class vehicle {
+    void dispp(){
+        System.out.print("I am vehicle class");
+    }
+}
+class cycle extends vehicle{
+    void disp1(){
+        System.out.println("i am a cycle class");
+
+    }
+
+}
+class truck extends vehicle{
+    void disp2(){
+        System.out.println("i am truck class");
+    }
+}
