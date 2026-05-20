f6c755990bb8cea268327c783fd90ad80b8ca6ad
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-20 23:04:00 +0530

    Create sumofaubarray.java

---
+++ sumofaubarray.java
@@ -0,0 +1,9 @@
+/ 30.Printing a sum of sub arrray
+static int sum;
+public static int   subarray(int arr[],int i,int sum,int k){
+   if(i==arr.length){
+       return (sum == k) ? 1 : 0;
+   }
+   return subarray(arr, i + 1, sum + arr[i], k)
+           + subarray(arr, i + 1, sum, k);
+}
