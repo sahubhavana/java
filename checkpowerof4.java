130da5ae6fe6ceea9ff0e9909f80fd7a25a69307
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-23 23:12:51 +0530

    Create checkpowerof4.java

---
+++ checkpowerof4.java
@@ -0,0 +1,8 @@
+ 7. to check that the a number is power of 4 or not
+       int n=sc.nextInt();
+       if((n&1)==0 && n!=2){
+           System.out.print("number is power of 4");
+       }
+       else{
+           System.out.print("number is not a power of 4");
+       }
