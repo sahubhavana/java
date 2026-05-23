eb521069bebe8c3650ad8f3388c76e12a162db25
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-23 23:29:44 +0530

    Create primebitwise.java

---
+++ primebitwise.java
@@ -0,0 +1,8 @@
+1.to check the number is either prime or even
+
+       if((a&1)==1){
+           System.out.print("number is odd");
+       }
+       if((a&1)==0){
+           System.out.print("number is even");
+       }
