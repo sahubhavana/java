ae5b170877af1b4faa3ee55a342d22b09eeb568d
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-24 22:15:08 +0530

    Create reversearry.java

---
+++ reversearry.java
@@ -0,0 +1,17 @@
+//  48.Reverse a array
+//        int arr[]={2,4,6,8,9};
+//        int i=0;
+//        int j=arr.length-1;
+//        while(i<arr.length/2){
+//            int temp=arr[i];
+//            arr[i]=arr[j];
+//            arr[j]=temp;
+//            if(i>j){
+//                break;
+//            }
+//            i++;
+//            j--;
+//        }
+//        for(int k=0;k<arr.length;k++){
+//            System.out.print(arr[k]+" ");
+//        }
