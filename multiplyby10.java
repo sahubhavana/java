c752bfa47db025d2f5c4ece7ab65b87b22b0a635
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-25 23:18:29 +0530

    Create multiplyby10.java

---
+++ multiplyby10.java
@@ -0,0 +1,10 @@
+
+  39.Multiply each element by 10
+
+       int arr[] = {1, 1, 2, 2, 3, 3, 4, 4};
+       for (int i = 0; i < arr.length; i++) {
+           arr[i] = arr[i] * 10;
+       }
+       for (int i = 0; i < arr.length; i++) {
+           System.out.print(arr[i]+" ");
+       }
