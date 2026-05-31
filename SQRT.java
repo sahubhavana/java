7af89a423867b017adbdf3a981efaf5566a2f1a4
BHAVANA SAHU <bvasahu@gmail.com>
2026-05-31 22:31:12 +0530

    Implement binary search for square root calculation

---
+++ SQRT.java
@@ -0,0 +1,21 @@
+public static  int SQRT(int x){
+        int start=0;
+        int end=x;
+        int ans=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(mid*mid==x){
+                return mid;
+            }
+            else{
+                if(mid*mid>x){
+                    end=mid-1;
+                }
+                else{
+                    ans=mid;
+                    start=mid+1;
+                }
+            }
+        }
+        return ans;
+ }
