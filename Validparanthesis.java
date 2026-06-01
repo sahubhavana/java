ecd69d6dd442acd84fd30f81af575df951b9e20c
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-01 22:55:31 +0530

    Create Validparanthesis.java

---
+++ Validparanthesis.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class Main {
+
+    public static boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            // Opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+
+            // Closing brackets
+            else {
+
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                char top = st.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+
+                    return false;
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "({[]})";
+
+        System.out.println(isValid(s));
+    }
+}
