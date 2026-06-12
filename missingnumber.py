3a852994d32d6d34c31d2592c820715cc8cc0aeb
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-12 23:20:19 +0530

    Create missingnumber.py

---
+++ missingnumber.py
@@ -0,0 +1,13 @@
+lass Solution {
+    public int missingNumber(int[] nums) {
+       
+        int ans=nums.length;
+        for(int i=0;i<nums.length;i++){
+          
+                 ans= ans^i^nums[i];
+                
+            }
+        
+        return ans;
+    }
+}
