c9c86f0677f06fe5c50c3dd073643ed32756d73a
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-20 12:46:22 +0530

    Create Bs.java

---
+++ Bs.java
@@ -0,0 +1,22 @@
+class Main {
+    static int binarySearch(int[] arr, int target) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9};
+        System.out.println(binarySearch(arr, 7));
+    }
+}
