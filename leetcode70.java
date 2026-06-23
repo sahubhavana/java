8123e1ba948facecd0fcf63ce86303fa40cd07a7
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-23 11:57:55 +0530

    Implement recursive solution for climbing stairs

---
+++ leetcode70.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        int ans=climbStairs(n-1)+climbStairs(n-2);
+        return ans;
+    }
+}
