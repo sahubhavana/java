ce6aca0d9f52a752202a0029ba5b18f6e0d51056
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-27 12:51:57 +0530

    Create fibonacinumber.java

---
+++ fibonacinumber.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int n) {
+        //your code goes here
+        if(n==0) return 0;
+        if(n==1) return 1;
+
+        int x=fib(n-1)+fib(n-2);
+        return x;
+    }
+}
