b6a5da15b798c6b2d1bcdd67e04db60295796163
BHAVANA SAHU <bvasahu@gmail.com>
2026-06-28 12:49:55 +0530

    Create reverseword2.java

---
+++ reverseword2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+       String[] words = s.trim().split("\\s+");
+        String s1 = "";
+        for (int i = words.length - 1; i >= 0; i--) {
+            s1 = s1 + words[i];
+            if (i != 0) {
+             s1 = s1 + " ";
+    }
+        }
+       return s1;
+    }
+}
