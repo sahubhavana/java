ff294c7a88121995c31900ccf19a1d52605845ad
BHAVANA SAHU <bvasahu@gmail.com>
2026-07-02 12:13:17 +0530

    Create sortstack.java

---
+++ sortstack.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public void sortStack(Stack<Integer> st) {
+
+        if (st.isEmpty())
+            return;
+
+        int x = st.pop();
+
+        sortStack(st);
+
+        sortedInsert(st, x);
+    }
+
+    public void sortedInsert(Stack<Integer> st, int x) {
+
+        if (st.isEmpty() || st.peek() <= x) {
+            st.push(x);
+            return;
+        }
+
+        int temp = st.pop();
+
+        sortedInsert(st, x);
+
+        st.push(temp);
+    }
+}
