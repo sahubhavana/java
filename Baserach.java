85c638dbc77b44fa4ca7297159c1a99f570e4245
BHAVANA SAHU <bvasahu@gmail.com>
2026-07-12 22:52:58 +0530

    Create Baserach.java

---
+++ Baserach.java
@@ -0,0 +1,23 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8, 10, 12};
+        int target = 8;
+
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                System.out.println("Found at index " + mid);
+                return;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        System.out.println("Not Found");
+    }
+}
