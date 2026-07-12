c0e5a1cad780e39fb0555d5daa521635ea7a5d1f
BHAVANA SAHU <bvasahu@gmail.com>
2026-07-12 22:51:28 +0530

    Create Mzero.java

---
+++ Mzero.java
@@ -0,0 +1,10 @@
+public static void moveZeroes(int[] nums) {
+    int index = 0;
+
+    for (int num : nums)
+        if (num != 0)
+            nums[index++] = num;
+
+    while (index < nums.length)
+        nums[index++] = 0;
+}
